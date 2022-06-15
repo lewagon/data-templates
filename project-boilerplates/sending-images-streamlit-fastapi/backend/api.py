541a9358ec9bc486113debbc3a916dc5327dda61
Pavel Liser <liser.pavel@gmail.com>
2022-06-15 14:42:34 +0800

    cleanup requirements and API CORS

--- project-boilerplates/sending-images-streamlit-fastapi/backend/api.py
+++ project-boilerplates/sending-images-streamlit-fastapi/backend/api.py
@@ -8,13 +8,14 @@ from starlette.responses import StreamingResponse
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
-)
+# # Allow all requests (optional)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],  # Allows all origins
+#     allow_credentials=True,
+#     allow_methods=["*"],  # Allows all methods
+#     allow_headers=["*"],  # Allows all headers
+# )
 
 @app.get("/")
 def index():
